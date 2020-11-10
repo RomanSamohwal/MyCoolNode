141fe9ae905b76547140249edaa7160ffe85772a
RomanSamohwal <rsamohwal@gmail.com>
2020-11-10 20:03:15 +0300

    connect MongoDB Atlas

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const http = require('http');
 const port = process.env.PORT || 3000
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb+srv://roman:mongodb@cluster0.qombh.mongodb.net/easycode?retryWrites=true&w=majority',{useNewUrlParser: true}).then(()=>{
+    console.log("MongoDB connected")
+}).then(()=>{
+    console.log("MongoDB connected")
+})
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
